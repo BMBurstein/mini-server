be8e93113ec6ca6484b5c1914405a3cfce8e513a
Baruch Burstein <bmburstein@gmail.com>
2017-02-08 19:06:29 +0200

    Add file to help test that headers are self-contained

---
+++ inc_test.cpp
@@ -0,0 +1,12 @@
+//#include "bitmask.hpp"
+//#include "methods.hpp"
+//#include "router.hpp"
+//#include "connection.hpp"
+//#include "server.hpp"
+// 
+// #include "connection_base.hpp"
+// struct C : bb::http_connection_base<C> {
+// 	bool handle_error(asio::error_code const&) {};
+// 	void handle_body() {};
+// 	void handle_parse_error() {};
+// };
