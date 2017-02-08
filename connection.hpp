519d4c57cc844a5d0a9c0e0e5d7b378fade9f887
Baruch Burstein <bmburstein@gmail.com>
2017-02-08 19:05:11 +0200

    Seperate generic HTTP connection stuff from server-specific part.

--- connection.hpp
+++ connection.hpp
@@ -2,7 +2,6 @@
 
 #include <chrono>
 #include <iostream>
-#include <map>
 #include <memory>
 #include <regex>
 #include <sstream>
@@ -11,40 +10,17 @@
 
 #include "asio.hpp"
 
+#include "connection_base.hpp"
+
 namespace bb {
 	class Router;
 
-	class Connection : public std::enable_shared_from_this<Connection>
+	class Connection : public http_connection_base<Connection>
 	{
-	private:
-		// source: http://stackoverflow.com/a/1801913/331785
-		struct IgnoreCaseLT {
-			// case-independent (ci) compare_less binary function
-			struct nocase_compare
-			{
-				bool operator() (const unsigned char& c1, const unsigned char& c2) const {
-					return tolower(c1) < tolower(c2);
-				}
-			};
-			bool operator() (const std::string & s1, const std::string & s2) const {
-				return std::lexicographical_compare(s1.begin(), s1.end(),   // source range
-				                                    s2.begin(), s2.end(),   // dest range
-				                                    nocase_compare());      // comparison
-			}
-		};
-
 	public:
-		typedef std::shared_ptr<Connection> ptr;
-		typedef std::map<std::string, std::string, IgnoreCaseLT> HeaderMap;
-		typedef std::vector<char> DATA;
-
 		template<typename ... T>
 		static ptr new_connection(T&& ... all) {
 			return std::shared_ptr<Connection>(new Connection(std::forward<T>(all)...));
-		}
-
-		~Connection() {
-			shutdown();
 		}
 
 		void start() { get_req(); }
@@ -77,18 +53,10 @@ namespace bb {
 			send_response(std::move(resp_buf));
 		}
 
-		HeaderMap const& getHeaders() { return req_headers; }
-		DATA const& getBody() { return req_body; }
-
 	private:
-		Connection(asio::ip::tcp::socket socket, Router const& router) : socket(std::move(socket)), router(router) { }
+		friend http_connection_base<Connection>;
 
-		void shutdown()
-		{
-			asio::error_code ec;
-			socket.shutdown(asio::ip::tcp::socket::shutdown_both, ec);
-			socket.close(ec);
-		}
+		Connection(asio::ip::tcp::socket socket, Router const& router) : http_connection_base(std::move(socket)), router(router) { }
 
 		void get_req() {
 			asio::async_read_until(socket, buf_in, "\r\n", [this, self{ shared_from_this() }](auto ec, auto) {
@@ -110,29 +78,7 @@ namespace bb {
 			});
 		}
 
-		void get_headers() {
-			asio::async_read_until(socket, buf_in, "\r\n", [this, self{ shared_from_this() }](auto ec, auto) {
-				if (handle_error(ec)) {
-					std::istream is(&buf_in);
-					std::string line;
-					std::getline(is, line);
-
-					std::smatch parts;
-					if (std::regex_match(line, parts, re_head)) {
-						req_headers[parts[1]] = parts[2];
-						get_headers();
-					}
-					else if (line == "\r") {
-						get_body();
-					}
-					else {
-						respond(400);
-					}
-				}
-			});
-		}
-
-		void get_body();
+		void handle_body();
 
 		void respond(int stat) {
 			send_response(stat, "", "");
@@ -150,61 +96,40 @@ namespace bb {
 			return false;
 		}
 
+		void handle_parse_error() {
+			respond(400);
+		}
+
 		void make_test_response() {
 			std::ostringstream os;
 			os << method << ' ' << uri << '\n';
-			for (const auto& h : req_headers) {
+			for (const auto& h : rcv_headers) {
 				os << h.first << " = " << h.second << '\n';
 			}
-			os << "Body length: " << req_body.size() << '\n';
+			os << "Body length: " << rcv_body.size() << '\n';
 			os << std::hex;
-			for (const auto c : req_body) {
+			for (const auto c : rcv_body) {
 				os << c;
 			}
 			send_response(404, "", os.str());
 		}
 
 		std::string method, uri;
-		HeaderMap req_headers;
-		DATA req_body;
-
-		asio::streambuf buf_in;
-		asio::ip::tcp::socket socket;
 		Router const& router;
 
 		const static std::regex re_req;
-		const static std::regex re_head;
 	}; // class Connection
 } // namespace bb
 
 #include "router.hpp"
 
 namespace bb {
-	inline void Connection::get_body() {
-		std::size_t len = 0;
-		auto it = req_headers.find("content-length");
-		if (it != req_headers.end()) {
-			try {
-				len = std::stoul(it->second.c_str());
-			}
-			catch (const std::exception&) {}
+	inline void Connection::handle_body() {
+		if (!router.handle_route(uri, method, shared_from_this())) {
+			make_test_response();
 		}
-		req_body.resize(len);
-		auto have_len = buf_in.size();
-		asio::buffer_copy(asio::buffer(req_body), buf_in.data());
-		buf_in.consume(have_len);
-		auto body_buf = asio::buffer(req_body.data() + have_len, len - have_len);
-
-		asio::async_read(socket, body_buf, [this, self{ shared_from_this() }](auto ec, auto) {
-			if (handle_error(ec)) {
-				if (!router.handle_route(uri, method, std::move(self))) {
-					make_test_response();
-				}
-			}
-		});
 	}
 
 	// The '\r' at the end is needed since getline only strips the '\n'
 	const std::regex Connection::re_req(R"(([A-Z]+) ([-/%.+?=&\w]+) HTTP/1.[10]\r)", std::regex::optimize);
-	const std::regex Connection::re_head(R"(([-\w]+): (.*)\r)", std::regex::optimize);
 } // namespace bb
---
+++ connection_base.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <regex>
+
+#include "asio.hpp"
+
+namespace bb {
+
+	template<typename T>
+	class http_connection_base : protected std::enable_shared_from_this<T> {
+	protected:
+		// source: http://stackoverflow.com/a/1801913/331785
+		struct IgnoreCaseLT {
+			// case-independent (ci) compare_less binary function
+			struct nocase_compare
+			{
+				bool operator() (const unsigned char& c1, const unsigned char& c2) const {
+					return tolower(c1) < tolower(c2);
+				}
+			};
+			bool operator() (const std::string & s1, const std::string & s2) const {
+				return std::lexicographical_compare(s1.begin(), s1.end(),   // source range
+					s2.begin(), s2.end(),   // dest range
+					nocase_compare());      // comparison
+			}
+		};
+
+	public:
+		typedef std::map<std::string, std::string, IgnoreCaseLT> HeaderMap;
+		typedef std::vector<char> DATA;
+		typedef std::shared_ptr<T> ptr;
+
+		HeaderMap const& headers() { return rcv_headers; }
+		DATA const& body() { return rcv_body; }
+
+	protected:
+		HeaderMap rcv_headers;
+		DATA rcv_body;
+
+		asio::streambuf buf_in;
+		asio::ip::tcp::socket socket;
+
+		http_connection_base(asio::ip::tcp::socket socket) : socket(std::move(socket)) { }
+
+		void get_headers() {
+			asio::async_read_until(socket, buf_in, "\r\n", [this, self{ shared_from_this() }](auto ec, auto) {
+				if (self->handle_error(ec)) {
+					std::istream is(&buf_in);
+					std::string line;
+					std::getline(is, line);
+
+					std::smatch parts;
+					if (std::regex_match(line, parts, re_head)) {
+						rcv_headers[parts[1]] = parts[2];
+						get_headers();
+					}
+					else if (line == "\r") {
+						get_body();
+					}
+					else {
+						self->handle_parse_error();
+					}
+				}
+			});
+		}
+
+		void get_body() {
+			std::size_t len = 0;
+			auto it = rcv_headers.find("content-length");
+			if (it != rcv_headers.end()) {
+				try {
+					len = std::stoul(it->second.c_str());
+				}
+				catch (const std::exception&) {}
+			}
+			rcv_body.resize(len);
+			auto have_len = buf_in.size();
+			asio::buffer_copy(asio::buffer(rcv_body), buf_in.data());
+			buf_in.consume(have_len);
+			auto body_buf = asio::buffer(rcv_body.data() + have_len, len - have_len);
+
+			asio::async_read(socket, body_buf, [this, self{ shared_from_this() }](auto ec, auto) {
+				if (self->handle_error(ec)) {
+					self->handle_body();
+				}
+			});
+		}
+
+		const static std::regex re_head;
+	};
+
+	// The '\r' at the end is needed since getline only strips the '\n'
+	template<typename T>
+	const std::regex http_connection_base<T>::re_head(R"(([-\w]+): (.*)\r)", std::regex::optimize);
+}
