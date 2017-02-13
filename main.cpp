b893d9c2980950af3010f4f26c5ecf348695654b
Baruch Burstein <bmburstein@gmail.com>
2017-02-13 09:08:15 +0200

    Use client_connection.hpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 
 #define ASIO_STANDALONE 1
 #define ASIO_NO_DEPRECATED 1
+
 #include "server.hpp"
+#include "client_connection.hpp"
 
 using namespace bb;
 
@@ -24,6 +26,17 @@ int main()
 	s.add_route("/", Methods::GET, responder);
 	s.add_route("/home", Methods::GET | Methods::POST, home);
 	s.add_route("/home2", Methods::POST, responder);
+	s.add_route("/fwd/([^/:]+)(:([0-9]+))?(/.*)", Methods::GET, [&](std::smatch const& path, Methods method, Connection::ptr con) {
+		try {
+			std::string port = path[3];
+			ClientConnection::new_connection(s.context(), path[1], (!port.empty() ? port : "http"))->send_request(path[4], "", [con{ std::move(con) }](ClientConnection::ptr ptr) {
+				con->send_response(ptr->status(), "", "");
+			});
+		}
+		catch (std::error_code const& ec) {
+			std::cout << ec.message();
+		}
+	});
 
 	s.run(std::thread::hardware_concurrency());
 
