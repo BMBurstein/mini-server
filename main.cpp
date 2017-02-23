03c2bf375c9102411aef47e45a69e239edb581c3
Baruch Burstein <bmburstein@gmail.com>
2017-02-23 17:32:31 +0200

    Forgot to commit all of the previous change

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@
 using namespace bb;
 
 void home(std::smatch const& path, Methods method, Connection::ptr con) {
-	con->send_response(200, "", "Hi from home() on " + path[0].str() + '\n');
+	con->make_response(200, "", "Hi from home() on " + path[0].str() + '\n');
 }
 
 struct Response
 {
 	void operator()(std::smatch const& path, Methods method, Connection::ptr con) {
-		con->send_response(200, "", "Hi from Response on " + path[0].str() + '\n');
+		con->make_response(200, "", "Hi from Response on " + path[0].str() + '\n');
 	}
 };
 
@@ -26,16 +26,16 @@ int main()
 	s.add_route("/", Methods::GET, responder);
 	s.add_route("/home", Methods::GET | Methods::POST, home);
 	s.add_route("/home2", Methods::POST, responder);
-	s.add_route("/fwd/([^/:]+)(:([0-9]+))?(/.*)", Methods::GET, [&](std::smatch const& path, Methods method, Connection::ptr con) {
-		try {
-			std::string port = path[3];
-			ClientConnection::new_connection(s.context(), path[1], (!port.empty() ? port : "http"))->send_request(path[4], "", [con{ std::move(con) }](ClientConnection::ptr ptr) {
-				con->send_response(ptr->status(), "", "");
+	s.add_route("/fwd/([^/:]+)(:([0-9]+))?(/.*)", Methods::GET, [&s](std::smatch const& path, Methods method, Connection::ptr con) {
+		std::string port = path[3];
+		ClientConnection::new_connection(s.context(), path[1], (!port.empty() ? port : "http"))->send_request(path[4], "", [con{ std::move(con) }, path, &s](ClientConnection::ptr ptr) {
+			asio::steady_timer tmr(s.context());
+			tmr.expires_after(std::chrono::seconds(7));
+			tmr.wait();
+			ptr->send_request(path[4], "", [con{ std::move(con) }](ClientConnection::ptr ptr) {
+				con->make_response(ptr->status(), ptr->headers(), ptr->body());
 			});
-		}
-		catch (std::error_code const& ec) {
-			std::cout << ec.message();
-		}
+		});
 	});
 
 	s.run(std::thread::hardware_concurrency());
